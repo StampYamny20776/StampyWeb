c8202a228a71f05e94136e7484c77744ac123d1e
StampYamny20776 <stfur750@gmail.com>
2025-02-03 17:57:32 -0500

    stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -58,3 +58,26 @@ export default defineStackbitConfig({
       .filter(Boolean) as SiteMapEntry[];
   }
 });
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  // ...
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
+});
